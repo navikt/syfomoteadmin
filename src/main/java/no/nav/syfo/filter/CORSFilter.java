c3f88c4045a45c64c78f9e472b3541d3eaf025c7
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2019-02-18 14:40:13 +0100

    Whitelister CORS-requests

---
+++ src/main/java/no/nav/syfo/filter/CORSFilter.java
@@ -0,0 +1,49 @@
+package no.nav.syfo.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.System.getProperty;
+
+public class CORSFilter implements Filter {
+
+    private List<String> whitelistedUrls = Arrays.asList(getProperty("WHITELIST_URL").split(","));
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+
+        if (!requestUriErMotFeedEllerInternalEndepunkt(httpRequest.getRequestURI())) {
+            String origin = httpRequest.getHeader("Origin");
+            if (originErWhitelisted(origin, httpRequest.getRequestURI())) {
+                httpResponse.setHeader("Access-Control-Allow-Origin", origin);
+                httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+                httpResponse.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, NAV_CSRF_PROTECTION");
+                httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            }
+        }
+
+        filterChain.doFilter(servletRequest, httpResponse);
+    }
+
+    @Override
+    public void destroy() {}
+
+
+    private boolean originErWhitelisted(String origin, String uri) {
+        return whitelistedUrls.contains(origin);
+
+    }
+
+    private boolean requestUriErMotFeedEllerInternalEndepunkt(String requestUrl) {
+        return requestUrl.contains("/api/system") || requestUrl.contains("/internal");
+    }
+}
