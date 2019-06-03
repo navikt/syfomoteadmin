71f3ab5a213da6bd42ed03295063d3f411801ba9
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-06-03 09:54:21 +0200

    Config TasKScheduler eksplisit
    Slipper implisitt config som gir No TaskScheduler/ScheduledExecutorService bean found for scheduled processing

--- src/main/java/no/nav/syfo/batch/scheduler/CacheEvictionScheduler.java
+++ src/main/java/no/nav/syfo/batch/scheduler/CacheEvictionScheduler.java
@@ -1,13 +1,11 @@
 package no.nav.syfo.batch.scheduler;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 
-@Slf4j
 @Component
 public class CacheEvictionScheduler {
 
--- src/main/java/no/nav/syfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/syfo/config/ApplicationConfig.java
@@ -2,11 +2,11 @@ package no.nav.syfo.config;
 
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.*;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +24,11 @@ public class ApplicationConfig {
     @Bean
     FlywayMigrationStrategy flywayMigrationStrategy(final JtaTransactionManager jtaTransactionManager) {
         return Flyway::migrate;
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
     }
 
     @Bean
