a95954766134876ffc5dd7c6a37357db0f6bad36
John Martin Lindseth <john.martin.lindseth@nav.no>
2019-06-21 16:12:19 +0200

    Legg til proxy-greier

---
+++ src/main/java/no/nav/syfo/config/AzureAdTokenConfig.java
@@ -0,0 +1,16 @@
+package no.nav.syfo.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AzureAdTokenConfig {
+    @Bean
+    RestTemplate restTemplateMedProxy() {
+        return new RestTemplateBuilder()
+                .additionalCustomizers(new NaisProxyCustomizer())
+                .build();
+    }
+}
