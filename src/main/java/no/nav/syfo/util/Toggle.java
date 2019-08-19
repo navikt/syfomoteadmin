6a627c554674df64a1e2114df80fcc1e9f5dbff4
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-08-19 12:03:52 +0200

    Remove unused toggles

--- src/main/java/no/nav/syfo/util/Toggle.java
+++ src/main/java/no/nav/syfo/util/Toggle.java
@@ -6,29 +6,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class Toggle {
 
-    private boolean toggleSendeEpost;
     private boolean toggleBatchPaaminelse;
-    private boolean toggleBatchEpost;
 
     public Toggle(
-            @Value("${toggles.send.epost:true}") boolean toggleSendeEpost,
-            @Value("${toggle.enable.batch.paaminnelse:false}") boolean toggleBatchPaaminelse,
-            @Value("${toggle.enable.batch.epost:false}") boolean toggleBatchEpost
+            @Value("${toggle.enable.batch.paaminnelse:true}") boolean toggleBatchPaaminelse
     ) {
-        this.toggleSendeEpost = toggleSendeEpost;
         this.toggleBatchPaaminelse = toggleBatchPaaminelse;
-        this.toggleBatchEpost = toggleBatchEpost;
-    }
-
-    public boolean toggleSendeEpost() {
-        return toggleSendeEpost;
     }
 
     public boolean toggleBatchPaaminelse() {
         return toggleBatchPaaminelse;
-    }
-
-    public boolean toggleBatchEpost() {
-        return toggleSendeEpost;
     }
 }
