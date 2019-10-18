fe5b3d479853e27ff0eadbb241ca034a0d404644
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-10-18 12:53:48 +0200

    Bugfix veilederoppgaver link

--- src/main/java/no/nav/syfo/util/RestUtils.java
+++ src/main/java/no/nav/syfo/util/RestUtils.java
@@ -4,15 +4,17 @@ import static java.lang.System.getenv;
 
 public class RestUtils {
 
+    private final static String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
+
     public static String baseUrl() {
         return "https://app" + miljo() + ".adeo.no";
     }
 
     private static String miljo() {
-        String environmentName = getenv("FASIT_ENVIRONMENT_NAME");
-        if ("p".equals(environmentName)) {
-            return "";
+        String environmentName = getenv(NAIS_CLUSTER_NAME);
+        if ("dev-fss".equals(environmentName)) {
+            return "-q1";
         }
-        return "-" + environmentName;
+        return "";
     }
 }
