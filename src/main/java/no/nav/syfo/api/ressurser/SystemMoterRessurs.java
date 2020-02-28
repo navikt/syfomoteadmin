08d5a653bac80185939b0c0354f215ebcf9e9f01
Tor-Christian Tangenes <torchrct@gmail.com>
2020-02-28 10:35:41 +0100

    Handle STS-tokens from syfomotebehov

--- src/main/java/no/nav/syfo/api/ressurser/SystemMoterRessurs.java
+++ src/main/java/no/nav/syfo/api/ressurser/SystemMoterRessurs.java
@@ -1,6 +1,6 @@
 package no.nav.syfo.api.ressurser;
 
-import no.nav.security.oidc.api.ProtectedWithClaims;
+import no.nav.security.oidc.api.Protected;
 import no.nav.syfo.service.MoteBrukerService;
 import no.nav.syfo.util.Brukerkontekst;
 import org.springframework.web.bind.annotation.*;
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.inject.Inject;
 import java.time.LocalDateTime;
 
-import static no.nav.syfo.oidc.OIDCIssuer.INTERN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
@@ -25,7 +24,7 @@ public class SystemMoterRessurs {
     }
 
     @PostMapping(produces = APPLICATION_JSON_VALUE, value = "/{aktorId}/harAktivtMote")
-    @ProtectedWithClaims(issuer = INTERN)
+    @Protected
     public boolean hentOmMoteErOpprettetEtterDato(
             @PathVariable("aktorId") String aktorId,
             @RequestBody LocalDateTime dato) {
