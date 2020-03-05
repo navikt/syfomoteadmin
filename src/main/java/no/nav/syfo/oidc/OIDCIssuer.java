a1e66f624a979489eef6b0f8abd602d8322f2b9b
Tor-Christian Tangenes <40393162+torchr89@users.noreply.github.com>
2020-03-05 08:14:28 +0100

    Handle STS-tokens from syfomotebehov (#46)

--- src/main/java/no/nav/syfo/oidc/OIDCIssuer.java
+++ src/main/java/no/nav/syfo/oidc/OIDCIssuer.java
@@ -4,4 +4,5 @@ public class OIDCIssuer {
     public final static String AZURE = "veileder";
     public final static String INTERN = "intern";
     public final static String EKSTERN = "selvbetjening";
+    public final static String STS = "sts";
 }
