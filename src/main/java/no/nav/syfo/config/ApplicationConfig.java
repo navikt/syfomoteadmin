a694a132b5f5b58382aca7f282b06a1f4adeec92
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-03-10 12:03:27 +0100

    Add retry mechanism for requests to DKIF

--- src/main/java/no/nav/syfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/syfo/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.*;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 import static java.util.Arrays.asList;
 
 @Configuration
+@EnableRetry
 @EnableTransactionManagement
 @EnableScheduling
 @EnableAspectJAutoProxy
