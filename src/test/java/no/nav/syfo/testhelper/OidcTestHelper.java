d4705832b81c843d885979e67d3e4655a77a9a73
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-10-07 10:30:49 +0200

    Delete unused test methods

--- src/test/java/no/nav/syfo/testhelper/OidcTestHelper.java
+++ src/test/java/no/nav/syfo/testhelper/OidcTestHelper.java
@@ -1,6 +1,7 @@
 package no.nav.syfo.testhelper;
 
-import com.nimbusds.jwt.*;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import no.nav.security.oidc.context.*;
 import no.nav.security.oidc.test.support.JwtTokenGenerator;
 import no.nav.syfo.oidc.OIDCIssuer;
@@ -15,21 +16,12 @@ public class OidcTestHelper {
         settOIDCValidationContext(oidcRequestContextHolder, jwt, OIDCIssuer.AZURE);
     }
 
-    public static void loggInnBruker(OIDCRequestContextHolder oidcRequestContextHolder, String subject) {
-        SignedJWT jwt = JwtTokenGenerator.createSignedJWT(subject);
-        settOIDCValidationContext(oidcRequestContextHolder, jwt, OIDCIssuer.EKSTERN);
-    }
-
     private static void settOIDCValidationContext(OIDCRequestContextHolder oidcRequestContextHolder, SignedJWT jwt, String issuer) {
         TokenContext tokenContext = new TokenContext(issuer, jwt.serialize());
         OIDCClaims oidcClaims = new OIDCClaims(jwt);
         OIDCValidationContext oidcValidationContext = new OIDCValidationContext();
         oidcValidationContext.addValidatedToken(issuer, tokenContext, oidcClaims);
         oidcRequestContextHolder.setOIDCValidationContext(oidcValidationContext);
-    }
-
-    public static OIDCValidationContext lagOIDCValidationContextEkstern(String subject) {
-        return lagOIDCValidationContext(subject, OIDCIssuer.EKSTERN);
     }
 
     private static OIDCValidationContext lagOIDCValidationContext(String subject, String issuer) {
@@ -44,5 +36,4 @@ public class OidcTestHelper {
     public static void loggUtAlle(OIDCRequestContextHolder oidcRequestContextHolder) {
         oidcRequestContextHolder.setOIDCValidationContext(null);
     }
-
 }
