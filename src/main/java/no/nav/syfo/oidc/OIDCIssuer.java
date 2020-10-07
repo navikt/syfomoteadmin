5c40b61a42ec3786aab47f6b0411a4c52f0fd494
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-10-07 14:24:36 +0200

    Remove unused issuer intern(ISSO)

--- src/main/java/no/nav/syfo/oidc/OIDCIssuer.java
+++ src/main/java/no/nav/syfo/oidc/OIDCIssuer.java
@@ -2,7 +2,6 @@ package no.nav.syfo.oidc;
 
 public class OIDCIssuer {
     public final static String AZURE = "veileder";
-    public final static String INTERN = "intern";
     public final static String EKSTERN = "selvbetjening";
     public final static String STS = "sts";
 }
