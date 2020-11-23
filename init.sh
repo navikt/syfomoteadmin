ac1fef3796a310af2325eabc5997e5ea2c52a707
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-11-23 10:07:23 +0100

    Use oracle-secrects instead of syfomoteadmin-secrects for moteadminDB

--- init.sh
+++ init.sh
@@ -3,3 +3,7 @@ export CLIENT_SECRET=$(cat /secrets/azuread/syfomoteadmin/client_secret)
 
 export LDAP_USERNAME=$(cat /secrets/ldap/ldap/username)
 export LDAP_PASSWORD=$(cat /secrets/ldap/ldap/password)
+
+export SPRING_DATASOURCE_URL=$(cat /secrets/moteadmindb/config/jdbc_url)
+export SPRING_DATASOURCE_USERNAME=$(cat /secrets/moteadmindb/credentials/username)
+export SPRING_DATASOURCE_PASSWORD=$(cat /secrets/moteadmindb/credentials/password)
