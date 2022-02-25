fc4884c33187f10c5509b38ef760d7743df9e364
Erik Jans <erik.gunnar.jansen@nav.no>
2022-02-25 15:34:05 +0100

    Remove unused code for consuming AzureAD v1 (#137)
    AzureAD v1 is no longer in use and is deleted.

--- init.sh
+++ init.sh
@@ -1,6 +1,3 @@
-export CLIENT_ID=$(cat /secrets/azuread/syfomoteadmin/client_id)
-export CLIENT_SECRET=$(cat /secrets/azuread/syfomoteadmin/client_secret)
-
 export SRV_USERNAME=$(cat /secrets/serviceuser/syfomoteadmin/username)
 export SRV_PASSWORD=$(cat /secrets/serviceuser/syfomoteadmin/password)
 
